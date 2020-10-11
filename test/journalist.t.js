3496d74e2a5399719423adc2fd5787f472adc8a6
Alan Gutierrez <alan@prettyrobots.com>
2020-10-11 03:03:28 -0500

    Copy existing implementation from Strata.
    Use new Proof and add a current `.gitignore`. Build only with Node.js on
    Travis CI.
    
    Closes #50.
    See #47.

---
+++ test/journalist.t.js
@@ -0,0 +1,24 @@
+require('proof')(2, async okay => {
+    const fs = require('fs').promises
+    const path = require('path')
+
+    const Journalist = require('..')
+
+    const directory = path.join(__dirname, 'tmp', 'commit')
+    await fs.rmdir(directory, { recursive: true })
+    // TODO Make this `async` so we can create the directory.
+    const commit = new Journalist(directory)
+
+    // TODO Add some files to dodge.
+    // await fs.mkdir(path.join(directory, 'dir'))
+
+    const entry = await commit.writeFile('hello/world.txt', Buffer.from('hello, world'))
+    okay(entry, {
+        method: 'emplace',
+        filename: 'hello/world.txt',
+        overwrite: false,
+        hash: '4d0ea41d'
+    }, 'write file')
+    okay(await commit.filename('hello/world.txt'), 'commit/hello/world.txt', 'aliased')
+    await commit.rename('hello/world.txt', 'hello/world.pdf')
+})
